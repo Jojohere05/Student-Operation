e0c1d1c4f81cc1f4306d62df69b7b24f6e3be79c
Jojohere05 <2005jhotikaraja@gmail.com>
2025-03-03 23:29:02 +0530

    declare arraylist and scanner

--- StudentManager.java
+++ StudentManager.java
@@ -4,5 +4,12 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 public class StudentManager {
-    
+    private ArrayList<Student> students;
+    private Scanner scanner ;
+    public StudentManager() {
+    students=new ArrayList<>();
+    scanner=new Scanner(System.in);
+    }
+  
+
 }
